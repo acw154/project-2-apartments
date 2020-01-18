665f1f02faaa8df690484bc765b78eef96e5219a
JWulf7 <j.wulf@live.com>
2020-01-18 15:39:12 -0500

    more stuff

--- RevatureRealocator/src/main/java/com/revature/Driver.java
+++ RevatureRealocator/src/main/java/com/revature/Driver.java
@@ -1,18 +1,14 @@
 package com.revature;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.revature.models.User;
-import com.revature.repositories.IUserDAO;
 
 public class Driver {
 
 	public static void main(String[] args) {
 
-			ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
 			
-			IUserDAO dao = (IUserDAO) ac.getBean("userDAO");
 
 		}
 }
