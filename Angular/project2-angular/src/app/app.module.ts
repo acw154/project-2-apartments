4a70437c483343fa37b897ad31ed6f88666717bb
OlesyaDud <wodadevelopment@gmail.com>
2020-01-21 10:25:33 -0500

    adding changes again, want to see how this works

--- Angular/project2-angular/src/app/app.module.ts
+++ Angular/project2-angular/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { LoginwordsComponent } from './components/loginwords/loginwords.componen
 import { LoginpageComponent } from './components/loginpage/loginpage.component';
 import { RentalsearchComponent } from './components/rentalsearch/rentalsearch.component';
 import { Background2Component } from './components/background2/background2.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { Background2Component } from './components/background2/background2.compo
     LoginwordsComponent,
     LoginpageComponent,
     RentalsearchComponent,
-    Background2Component
+    Background2Component,
+    ProfileComponent,
+    RegisterComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
