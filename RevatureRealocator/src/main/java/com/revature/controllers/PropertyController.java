c6087d8bf1cbf18564036a4cf6c1c3de7aefd8ee
acw154 <acw154@gmail.com>
2020-01-22 10:56:33 -0500

    Changed propertycontroller to a RESTController

--- RevatureRealocator/src/main/java/com/revature/controllers/PropertyController.java
+++ RevatureRealocator/src/main/java/com/revature/controllers/PropertyController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.models.Preference;
 import com.revature.models.Property;
@@ -16,6 +17,7 @@ import com.revature.services.APIParse;
 import com.revature.services.PropertyService;
 import com.revature.util.APIUtil;
 
+@RestController
 public class PropertyController {
 	@Autowired 
 	private PropertyService ps;
@@ -26,31 +28,38 @@ public class PropertyController {
 	
 	@CrossOrigin(origins="http://localhost:4200")
 	@PostMapping("/propsearch")
-	@ResponseBody
-	public ResponseEntity<List<Property>> getProperties(@RequestBody Preference pref){
+	public List<Property> getProperties(@RequestBody Preference pref){
 		//Get POST request values and create a preference object to create an API call with
 		String query = api.createQueryByPreference(pref);
 		List<Property> list = APIParse.parse(api.getResponse(query).toString()); //TODO: Map the response body to Properties and return them
-		return ResponseEntity.ok().body(list);
+		return list;
 	}
 	
 	@CrossOrigin(origins="http://localhost:4200")
 	@PostMapping("/propsearch")
-	@ResponseBody
-	public ResponseEntity<List<Property>> getPropertiesSimpleSearch(@RequestBody SimpleSearchBody ssb){
+	public List<Property> getPropertiesSimpleSearch(@RequestBody SimpleSearchBody ssb){
 		String state_code = ssb.getState_code();
 		String city = ssb.getCity();
 		String query = api.createSimpleQuery(state_code, city);
 		List<Property> list = APIParse.parse(api.getResponse(query).toString());
-		return ResponseEntity.ok().body(list);
+		return list;
 	}
 	
 	@CrossOrigin(origins="http://localhost:4200")
 	@PostMapping("/propsearch")
 	@ResponseBody
-	public ResponseEntity<List<Property>> getPropertiesSimpleSearch(@RequestBody Preference pref){
+	public List<Property> getPropertiesSimpleSearch(@RequestBody Preference pref){
 		String query = api.createSimpleQuery(pref);
 		List<Property> list = APIParse.parse(api.getResponse(query).toString()); //TODO: Map the response body to Properties and return them
-		return ResponseEntity.ok().body(list);
+		return list;
+	}
+	
+	@CrossOrigin(origins="http://localhost:4200")
+	@PostMapping("/propsearch")
+	public List<Property> getProperties(String state_code, String city){
+		//Get POST request values and create a preference object to create an API call with
+		String query = api.createSimpleQuery(state_code, city);
+		List<Property> list = APIParse.parse(api.getResponse(query).toString()); //TODO: Map the response body to Properties and return them
+		return list;
 	}
 }
