ca5827019dc754dec960836cf76998fc4fce3840
JWulf7 <j.wulf@live.com>
2020-01-22 21:26:07 -0500

    successful HTTP requests from controllers to DB and back for Users

--- RevatureRealocator/src/main/java/com/revature/services/UserService.java
+++ RevatureRealocator/src/main/java/com/revature/services/UserService.java
@@ -7,42 +7,68 @@ import org.springframework.stereotype.Service;
 
 import com.revature.models.Property;
 import com.revature.models.User;
+import com.revature.models.UserStatus;
 import com.revature.repositories.UserDAO;
+import com.revature.repositories.UserStatusDAO;
 
 @Service
 public class UserService {
 	
-	public UserService(UserDAO dao) {
+	
+	public UserService() {
 		super();
-		this.dao = dao;
+	}
+
+	public UserService(UserDAO usdao) {
+		super();
+		this.usdao = usdao;
+	}
+	
+	public UserService(UserDAO usdao, UserStatusDAO ustatdao) {
+		super();
+		this.usdao = usdao;
+		this.ustatdao = ustatdao;
 	}
 
 	@Autowired
-	private UserDAO dao;
+	private UserDAO usdao;
+	@Autowired
+	private UserStatusDAO ustatdao;
 	
 	public List<User> findAll() {
-		return dao.findAll();
+		return usdao.findAll();
 	}
 	
 	public User findById(int id) {
-		return dao.findById(id);
+		return usdao.findById(id);
 	}
 	
 	public User findByEmail(String email) {
-		return dao.getUserByEmail(email);
+		return usdao.getUserByEmail(email);
 	}
 
 	
-	public boolean upsert(User u) {
-		dao.upsertUser(u);
-		if(dao.findById(u.getId()).equals(u)) {
-			return true;
+	public User upsert(User u) {
+		System.out.println("upsert method in UserService");
+		System.out.println(u.getId());
+		String userStatusString = u.getUserStatus().getStatus();
+		UserStatus fullStatus = grabActualUserStatus(u);
+		u.setUserStatus(fullStatus);
+		usdao.upsertUser(u);
+		System.out.println("Upserted this: " + u);
+		System.out.println("After, it is this: " + usdao.getUserByEmail(u.getEmail()));
+		if(usdao.getUserByEmail(u.getEmail()).equals(u)) {
+			System.out.println("inside if conditional, true, upsert method UserService");
+			
+			User user = usdao.getUserByEmail(u.getEmail());
+			return user;
 		}
-		return false;
+		System.out.println("did not hit if conditional, false, upsert method UserService");
+		return null;
 	}
 	
 	public boolean verifyUser(String email, String password) {
-		User u = dao.getUserByEmail(email);
+		User u = usdao.getUserByEmail(email);
 		if(u != null) {
 			if(u.getPassword().equals(password)) {
 				return true;
@@ -52,7 +78,15 @@ public class UserService {
 	}
 	
 	public List<User> findByState(String state){
-		return dao.findByState(state);
+		return usdao.findByState(state);
+	}
+	
+	
+	public UserStatus grabActualUserStatus(User user) {
+		String statusString = user.getUserStatus().getStatus();
+		UserStatus fullStatusObject = ustatdao.getStatusByString(statusString);
+		return fullStatusObject;
+		
 	}
 
 }
