db021d852582589702ef5d85213bc5a68e0f2820
acw154 <acw154@gmail.com>
2020-01-26 23:18:52 -0500

    given up on using branches

--- Angular/project2-angular/src/app/components/editprofile/editprofile.component.ts
+++ Angular/project2-angular/src/app/components/editprofile/editprofile.component.ts
@@ -27,10 +27,10 @@ export class EditprofileComponent implements OnInit {
   
   createForm(){
     this.editForm = this.fb.group({
-      min_price: ['', [Validators.required, Validators.min(0)]],
-      max_price: ['', [Validators.required, Validators.max(50000)]],
-      num_beds: ['', [Validators.required, Validators.min(0)]],
-      num_baths: ['', [Validators.required, Validators.min(0)]],
+      minPrice: ['', [Validators.required, Validators.min(0)]],
+      maxPrice: ['', [Validators.required, Validators.max(50000)]],
+      numBeds: ['', [Validators.required, Validators.min(0)]],
+      numBaths: ['', [Validators.required, Validators.min(0)]],
       city: ['', [Validators.required]],
       state_code: ['', [Validators.required]],
     })
@@ -48,7 +48,7 @@ export class EditprofileComponent implements OnInit {
     this.profileService.savePreference(this.preference).subscribe(
       data => {
         if(data != null){
-          console.log('Successfylly changed preferences');
+          console.log('Successfully changed preferences');
           this.sessionService.storePreference(data);
           alert('Successfylly changed preferences');
         } else {
